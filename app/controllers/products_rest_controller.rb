98404f59dcb3e917dfc63a44b843d0acea8bbe3b
pavel polushkin <pavel.polushkin@gmail.com>
2015-06-24 00:35:40 +0300

    some fixes

--- app/controllers/products_rest_controller.rb
+++ app/controllers/products_rest_controller.rb
@@ -8,8 +8,20 @@ class ProductsRestController  < ApplicationController
 
   #GET api/products
   def all_products
-    all_products = Product.where("article like 'В%'").order("price desc")
-    render json: all_products
+    all_products = Product.where("article like 'В%'").order("price desc").first(25)
+
+    formatted_products = Array.new
+    all_products.each do |p|
+      formatted_products << {
+          :id => p.id,
+          :name => p.name,
+          :price => sprintf("%u р", p.price),
+          :img => p.medium_img.url,
+          :size => p.width.to_s + 'x' + p.height.to_s + ' см'
+      }
+    end
+
+    render json: formatted_products
   end
 
 end
