b57952cff8be0daa60a9106f7e2c291f742b2fbd
pavel polushkin <pavel.polushkin@gmail.com>
2015-10-25 03:00:46 +0300

    improve put to basket action

--- app/assets/javascripts/store-controller.js
+++ app/assets/javascripts/store-controller.js
@@ -5,6 +5,7 @@
     StoreCtrl.$inject = ['$scope', '$route', '$routeParams', '$http', '$log'];
 
     function StoreCtrl($scope, $route, $routeParams, $http, $log) {
+
         $scope.goods_type = $scope.UTIL.getNavId($route, $routeParams);
 
         var url = 'api/products/?goods_type='+$scope.goods_type;
@@ -12,6 +13,22 @@
         $http.get(url).success(function (data) {
             $scope.products = data;
         });
+
+        $scope.putToBasket = function(product) {
+            if (product.amount == null) {
+                product.amount = 1;
+            } else {
+                product.amount += 1;
+            }
+        };
+
+        $scope.isInBasket = function(product) {
+            if (product.amount == null) {
+                return false;
+            } else {
+                return product.amount >0;
+            }
+        }
 
     }
 })();
