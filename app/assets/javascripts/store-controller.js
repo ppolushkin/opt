e330b883430c3cabcd84eb79fd764f24465646e7
pavel polushkin <pavel.polushkin@gmail.com>
2015-11-04 00:29:30 +0300

    optimization

--- app/assets/javascripts/store-controller.js
+++ app/assets/javascripts/store-controller.js
@@ -2,24 +2,38 @@
     "use strict";
     angular.module('obeliskControllers').controller('StoreCtrl', StoreCtrl);
 
-    StoreCtrl.$inject = ['basketService', '$scope', '$route', '$routeParams', '$http', '$log'];
+    StoreCtrl.$inject = ['basketService', '$scope', '$route', '$routeParams', '$http', '$log',  '$sessionStorage'];
 
-    function StoreCtrl(basketService, $scope, $route, $routeParams, $http, $log) {
+    function StoreCtrl(basketService, $scope, $route, $routeParams, $http, $log,  $sessionStorage) {
 
         $scope.init = function () {
+
+            var loadProducts = function(goods_type, onSuccess) {
+                var url = 'api/products/?goods_type=' + $scope.goods_type;
+                debugger;
+                if ($sessionStorage.products == undefined ||
+                    $sessionStorage.products[goods_type] == undefined
+                ) {
+                    $http.get(url).success(function (data) {
+                        $scope.products = data;
+                        $sessionStorage.products[goods_type] = data;
+
+                        onSuccess();
+                    });
+                } else {
+                    $scope.products = $sessionStorage.products[goods_type];
+                    onSuccess();
+                }
+            };
+
             $scope.goods_type = $scope.UTIL.getNavId($route, $routeParams);
 
-            var url = 'api/products/?goods_type=' + $scope.goods_type;
-
-            $http.get(url).success(function (data) {
-                $scope.products = data;
-
+            loadProducts($scope.goods_type, function() {
                 var l = $scope.products.length;
                 for (var i = 0; i < l; i++) {
                     var product = $scope.products[i];
                     product.amount = basketService.basket.getProductAmount(product.article);
                 }
-
             });
         };
 
