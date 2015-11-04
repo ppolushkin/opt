6008ed0633feeb4c35d16c390eebd805cc4ac36a
pavel polushkin <pavel.polushkin@gmail.com>
2015-11-05 00:38:43 +0300

    cache pages

--- app/assets/javascripts/page-controller.js
+++ app/assets/javascripts/page-controller.js
@@ -7,20 +7,29 @@
 
     angular.module('obeliskControllers').controller('PageCtrl', PageCtrl);
 
-    PageCtrl.$inject = ['$scope', '$routeParams', '$route', '$http', '$log'];
+    PageCtrl.$inject = ['$scope', '$routeParams', '$route', '$http', '$log', '$sessionStorage'];
 
-    function PageCtrl($scope, $routeParams, $route, $http, $log) {
+    function PageCtrl($scope, $routeParams, $route, $http, $log, $sessionStorage) {
 
-        $scope.page_id = $scope.UTIL.getNavId($route, $routeParams);
+        $scope.init = function () {
 
-        //$scope.content = "I'm page " + $scope.page_id;
+            $scope.page_id = $scope.UTIL.getNavId($route, $routeParams);
 
-        $http.get('api/revisions/last/'+ $scope.page_id).success(function (data) {
-            $scope.content = data.content;
-            $log.log('success');
-        });
+            if ($sessionStorage.pages == undefined) {
+                $sessionStorage.pages = {};
+            }
 
+            if ($sessionStorage.pages[$scope.page_id] == undefined) {
+                $http.get('api/revisions/last/'+ $scope.page_id).success(function (data) {
+                    $scope.content = data.content;
+                    $sessionStorage.pages[$scope.page_id] = data.content;
+                });
+            } else {
+                $scope.content = $sessionStorage.pages[$scope.page_id];
+            }
+        };
 
+        $scope.init();
     }
 })();
 
