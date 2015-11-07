630022cf82207d9f0cef0efb64d638c2929dcd8e
pavel polushkin <pavel.polushkin@gmail.com>
2015-11-08 02:12:53 +0300

    modal detailed view

--- app/assets/javascripts/order-controller.js
+++ app/assets/javascripts/order-controller.js
@@ -2,9 +2,9 @@
     "use strict";
     angular.module('obeliskControllers').controller('OrderCtrl', OrderCtrl);
 
-    OrderCtrl.$inject = ['basketService', '$scope', '$http', '$log'];
+    OrderCtrl.$inject = ['basketService', '$scope', '$http', '$log', '$modal'];
 
-    function OrderCtrl(basketService, $scope, $http, $log) {
+    function OrderCtrl(basketService, $scope, $http, $log, $modal) {
 
         $scope.init = function() {
             $scope.basket = basketService.basket;
@@ -18,6 +18,37 @@
             return $scope.basket.isEmpty();
         };
 
+        $scope.openDetailedView = function(product) {
+            //var modalInstance =
+            $modal.open({
+                templateUrl: '/partials/detailed-view.html',
+                animation: true,
+                size: 'sm',
+                controller: 'DetailedViewCtrl',
+                resolve: {
+                    product: function () {
+                        return product;
+                    }
+                }
+            });
+
+            //modalInstance.result.then(function (loggedIn) {
+            //    $scope.loggedIn = loggedIn;
+            //});
+
+        };
     }
+
+    angular.module('obeliskControllers').controller('DetailedViewCtrl', DetailedViewCtrl);
+
+    DetailedViewCtrl.$inject = ['$scope', '$modalInstance', '$http', '$log', 'product'];
+
+    function DetailedViewCtrl($scope, $modalInstance, $http, $log, product) {
+
+        $scope.product = product;
+
+
+    }
+
 })();
 
